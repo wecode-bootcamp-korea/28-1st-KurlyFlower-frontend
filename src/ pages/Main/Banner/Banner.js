41aa40b00a9f550a37e5b602b586e8ffe5e25f00
gy <gayunlee11@gmail.com>
2021-12-28 14:38:16 +0900

    [Add] 메인페이지 배너 무한 슬라이드 구현 - 무한 이동 기능 구현 완료 - 처음과 끝 도달 시 애니메이션 효과 없이 이동 후, 애니메이션 효과로 다음 슬라이드로 넘기는 기능 구현 중

--- src/ pages/Main/Banner/Banner.js
+++ src/ pages/Main/Banner/Banner.js
@@ -4,23 +4,71 @@ import { MdKeyboardArrowRight } from 'react-icons/md';
 import './Banner.scss';
 
 function Banner() {
+  const bannerContents = [
+    '/images/main/1.png',
+    '/images/main/2.png',
+    '/images/main/3.png',
+  ];
+
+  const ThreeTimesBannerContents = [
+    ...bannerContents,
+    ...bannerContents,
+    ...bannerContents,
+  ];
+
   const SLIDE_WIDTH = '1440';
   const slideRef = useRef();
 
-  let [slideNum, setSlideNum] = useState(0);
+  const BANNERS_COUNT = bannerContents.length;
+  const TOTAL_BANNERS_COUNT = BANNERS_COUNT * 3;
+  const START = (TOTAL_BANNERS_COUNT * 1) / 3 + 1; //4
+  const END = (TOTAL_BANNERS_COUNT * 2) / 3;
+  // const PREV_START = 1;
+  const PREV_END = (TOTAL_BANNERS_COUNT * 1) / 3; //3
+  const NEXT_START = (TOTAL_BANNERS_COUNT * 2) / 3 + 1; //7
+  // const NEXT_END = TOTAL_BANNERS_COUNT; //9
+
+  let [slide, setSlide] = useState({
+    number: START,
+    memo: 0,
+  });
 
   useEffect(() => {
+    console.log(slide.number);
     slideRef.current.style.transform = `translateX(-${
-      SLIDE_WIDTH * slideNum
+      SLIDE_WIDTH * (slide.number - 1)
     }px)`;
-  }, [slideNum]);
+    console.log(NEXT_START === slide.number);
+  }, [slide]);
 
   function onClickLeft() {
-    setSlideNum(slideNum => slideNum - 1);
+    if (slide.number === PREV_END && slide.memo === PREV_END + 1) {
+      setSlide({
+        number: END,
+        memo: END - 1,
+      });
+      console.log('time to move next');
+    } else {
+      setSlide({
+        number: slide.number - 1,
+        memo: slide.number,
+      });
+    }
   }
 
   function onClickRight() {
-    setSlideNum(slideNum => slideNum + 1);
+    if (slide.number === NEXT_START && slide.memo === NEXT_START - 1) {
+      setSlide({
+        number: START,
+        memo: START + 1,
+      });
+      console.log('time to move next');
+    } else {
+      setSlide({
+        number: slide.number + 1,
+        memo: slide.number,
+      });
+    }
   }
 
   return (
@@ -28,15 +76,11 @@ function Banner() {
       <div className="banner-wrap">
         <div className="banner-container">
           <div className="list" ref={slideRef}>
-            <article className="item">
-              <img className="item-img" src="/images/main/1.png" alt="" />
-            </article>
-            <article className="item">
-              <img src="/images/main/2.png" alt="" />
-            </article>
-            <article className="item">
-              <img src="/images/main/3.png" alt="" />
-            </article>
+            {ThreeTimesBannerContents.map(banner => (
+              <article className="item">
+                <img className="item-img" src={banner} alt="" />
+              </article>
+            ))}
           </div>
         </div>
         <div className="buttons">
