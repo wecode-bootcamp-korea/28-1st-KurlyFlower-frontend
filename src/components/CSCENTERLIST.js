27a3cf213c0bfaf46faff44123d3051f5df53077
한상일 <mint@hansang-il-ui-MacBookAir.local>
2021-12-29 09:29:13 +0900

    20211228 로그인페이지 레이아웃 완료

---
+++ src/components/CSCENTERLIST.js
@@ -0,0 +1,7 @@
+export const CSCENTERLIST = [
+  { id: 1, content: '공지사항' },
+  { id: 2, content: '자주하는 질문' },
+  { id: 3, content: '1:1 문의' },
+  { id: 4, content: '대량 주문 문의' },
+  { id: 5, content: '상품 제안' },
+];
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import NavLogo from '../ pages/Login/Navlogo';
+import { CSCENTERLIST } from './CSCENTERLIST';
 import './Nav.scss';
 
-function Nav() {
-  return <div>nav</div>;
-}
+const Nav = () => {
+  const [listOpen, setlistOpen] = useState(false);
+  const listToggleMenu = () => {
+    setlistOpen(listOpen => !listOpen);
+  };
+  return (
+    <div className="navLocation">
+      <NavLogo />
+      <a href="https://www.google.co.kr">
+        <div className="leftInfo">
+          <span className="Info"> 샛별·택배</span> 배송안내
+        </div>
+      </a>
+      <li className="rightMenu">
+        <Link to="/signup">
+          <li className="rightMenuli" id="rightList">
+            회원가입
+          </li>
+        </Link>
+        <Link to="/">
+          <li className="rightMenuli" id="rightList">
+            로그인
+          </li>
+        </Link>
+
+        <li
+          className="rightMenulist"
+          id="rightList"
+          onClick={() => listToggleMenu()}
+        >
+          고객센터
+          <ul className={listOpen ? 'show-menu' : 'hide-menu'}>
+            {CSCENTERLIST.map(CSList => {
+              <div>{CSList.id}</div>;
+              return <li className="listTest">{CSList.content}</li>;
+            })}
+          </ul>
+        </li>
+      </li>
+      <div className="navFooter">
+        <div className="categoriesGroup">
+          <img
+            src="https://img.icons8.com/material-outlined/24/000000/menu--v1.png"
+            className="imgCate"
+          />
+          <span className="categorieAll">전체 카테고리</span>
+          <span className="categorieMenuV">채소</span>
+          <span className="categorieMenuF">과일</span>
+          <span className="categorieMenuM">정육</span>
+          <span className="categorieMenuFi">수산</span>
+
+          <input
+            type="text"
+            placeholder="검색어를 입력해주세요"
+            className="searchField"
+          />
+          <img
+            src="https://img.icons8.com/material-outlined/24/000000/search--v1.png"
+            className="imgSearch"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Nav;
